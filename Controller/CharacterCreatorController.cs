61d3f174ae1c9967756afda3ac6bea788487836c
jonesarn <ajones10196@icloud.com>
2022-04-23 14:02:08 -0400

    Some of the controller stuff

---
+++ Controller/CharacterCreatorController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharacterCreator.Controllers
+{
+    public class CharacterCreatorController
+    {
+        public class TheCharacterCreatorController: Controller
+        {
+            
+        }
+    }
+}
