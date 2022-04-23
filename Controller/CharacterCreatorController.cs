caa84b2668b26c805056efbbdc2cb22a2d044e06
Griffin Brown <browgrif@gmail.com>
2022-04-23 14:26:56 -0400

    DB context set for Characters

--- Controller/CharacterCreatorController.cs
+++ Controller/CharacterCreatorController.cs
@@ -1,18 +1,30 @@
+using CharacterCreator.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CharacterCreator.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CharacterCreatorController
+    public class CharacterCreatorController : Controller
     {
-        public class TheCharacterCreatorController: Controller
+        private CharacterCreatorDbContext _context;
+        public CharacterCreatorController(CharacterCreatorDbContext context)
         {
-            private CharacterCreatorDbContext _context;
-            public TheCharacterCreatorController(CharacterCreatorDbContext context)
-            {
-                _context = context;
-            }
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCharacter([FromForm] CharacterEdit model)
+    {
+        Character character = new Character() {
+            Name = model.Name,
+            Ability = model.Ability,
+            Attack = model.Attack,
+            Defence = model.Defence,
+            Support = model.Defence
+        };
+        _context.Characters.Add(character);
+        await _context.SaveChangesAsync();
+        return Ok();
         }
     }
 }
